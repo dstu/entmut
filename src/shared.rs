1799b823e32241446ff75bbd1c6b0fbac0377e2e
Stu Black <trurl@freeshell.org>
2015-04-17 14:48:02 -0400

    Progress in getting shared into shape.

--- src/shared.rs
+++ src/shared.rs
@@ -1,4 +1,5 @@
 use ::{Guard, Nav};
+use ::util::{ChildIndex, SiblingIndex};
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -22,7 +23,7 @@ impl<'a, T: 'a> Guard<'a, RefCell<T>> for DataGuard<'a, T> {
 }
 
 pub struct Navigator<'a, T: 'a> {
-    here: &'a Tree<T>, path: Vec<&'a Tree<T>>,
+    here: &'a Tree<T>, path: Vec<(&'a Tree<T>, usize)>,
 }
 
 impl<'a, T: 'a> Nav<'a> for Navigator<'a, T> {
@@ -30,23 +31,47 @@ impl<'a, T: 'a> Nav<'a> for Navigator<'a, T> {
     type DataGuard = DataGuard<'a, T>;
 
     fn seek_sibling(&mut self, offset: isize) {
-        // TODO
+        let new_index = {
+            if self.at_root() {
+                SiblingIndex::Root
+            } else {
+                let (parent, here_index) = self.path[self.path.len() - 1];
+                SiblingIndex::compute(parent.internal.children.borrow().len(),
+                                      here_index,
+                                      offset)
+            }
+        }.unwrap();
+        let (parent, _) = self.path.pop().unwrap();
+        self.path.push((parent, new_index));
+        self.here = &parent.internal.children.borrow()[new_index];
     }
 
     fn seek_child(&mut self, index: usize) {
-        // TODO
+        let new_index =
+            ChildIndex::compute(self.here.internal.children.borrow().len(), index).unwrap();
+        self.path.push((self.here, new_index));
+        self.here = &self.here.internal.children.borrow()[new_index];
     }
 
     fn child_count(&self) -> usize {
         self.here.internal.children.borrow().len()
     }
 
-    fn is_root(&self) -> bool {
+    fn at_root(&self) -> bool {
         self.path.is_empty()
     }
 
     fn to_parent(&mut self) {
-        self.here = self.path.pop().expect("Already at root");
+        let (parent, _) = self.path.pop().expect("already at root");
+        self.here = parent;
+    }
+
+    fn to_root(&mut self) {
+        if ! self.at_root() {
+            let (root, _) = self.path[0];
+            self.here = root;
+            self.path.clear();
+        }
     }
 
     fn data(&self) -> DataGuard<'a, T> {
