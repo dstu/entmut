5542ac07d8f08934a7cb06e586f9a20514d91751
Stu Black <trurl@freeshell.org>
2015-07-18 09:22:26 -0400

    Tweak test definitions to clarify what's going on.

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,18 +1,15 @@
 #[macro_use(owned_tree, shared_tree)]
 extern crate entmut;
 
-// This will define macros for generalized tests of Nav and Editor impls.
-#[cfg(test)]
+/// Defines macros for generalized tests of Nav impls.
 #[macro_use]
 mod view_tests;
 
-#[cfg(test)]
 #[macro_use]
 mod owned {
     view_tests!(owned_tree);
 }
 
-#[cfg(test)]
 #[macro_use]
 mod shared {
     view_tests!(shared_tree);
